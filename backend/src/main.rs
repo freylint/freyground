a5a104163f6cc63e966a50d7dfa018b5f731eb83
freylint <lmpriestley@proton.me>
2022-11-09 23:34:47 -0600

    Change file search dir to build/static/

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,7 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::Request;
 use rocket::fs::NamedFile;
 use std::io;
 use std::path::Path;
@@ -16,14 +17,22 @@ async fn index() -> io::Result<NamedFile> {
 
 #[get("/<file..>")]
 async fn serve(file: PathBuf) -> io::Result<NamedFile> {
-    let prefix: PathBuf = "build/".into();
-    let path = prefix.join(file);
-    println!("{:?}", path);
-    NamedFile::open(path).await
+    NamedFile::open(Path::new("build/static/").join(file)).await
 }
+
+#[catch(500)]
+fn internal_error() -> &'static str {
+    "Internal server error: Error 500"
+}
+#[catch(400)]
+fn not_found(req: &Request) -> String {
+    format!("I couldn't find '{}'. Try something else?", req.uri())
+}
+
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, serve])
+        .register("/", catchers![internal_error, not_found],)
 }
