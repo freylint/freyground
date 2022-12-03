80ccdc9572dc8362b05ff7cb3ca6b9541a0ba2ab
freylint <lmpriestley@tuta.io>
2022-12-02 21:42:34 -0600

    Create basic shell of fg-runtime project.

--- projects/fg-runtime/src/main.rs
+++ projects/fg-runtime/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+//! Freyground microservices runtime
+//!
+//! This handles the loading, and execution of the various wasm components
+//! of the greater freyground repository.
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
 }
