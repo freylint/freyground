260d62e4d1dc8c598f8e275b3bce203239997c25
freylint <lmpriestley@tuta.io>
2022-12-02 20:42:14 -0600

    Refactor rtInit tests.

--- tests/rtInit.js
+++ tests/rtInit.js
@@ -4,15 +4,15 @@ const rtInit = loadFeature("features/rtInit.feature");
 
 defineFeature(rtInit, (test) => {
   test("Runtime has started", ({ given, then, and }) => {
-    given(/^Runtime can find (.*)$/, (arg0) => {
+    given(/^Runtime can find (.*)$/, (target) => {
       fail("Not yet implemented");
     });
 
-    then(/^Runtime loads (.*)$/, (arg0) => {
+    then(/^Runtime loads (.*)$/, (target) => {
       fail("Not yet implemented");
     });
 
-    and(/^Runtime starts (.*)$/, (arg0) => {
+    and(/^Runtime starts (.*)$/, (target) => {
       fail("Not yet implemented");
     });
   });
