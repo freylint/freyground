ded105cc1815854ef8ac2ca35442d8e4039c526e
freylint <lmpriestley@tuta.io>
2022-12-02 20:41:20 -0600

    Create placeholder runtime testing.

--- tests/runtime.steps.js
+++ tests/runtime.steps.js
@@ -3,9 +3,17 @@ import { loadFeature, defineFeature } from "jest-cucumber";
 const rtInit = loadFeature("features/rtInit.feature");
 
 defineFeature(rtInit, (test) => {
-  test("Runtime has started", ({ given, when, then }) => {});
+  test("Runtime has started", ({ given, then, and }) => {
+    given(/^Runtime can find (.*)$/, (arg0) => {
+      fail("Not yet implemented");
+    });
 
-  given("Runtime can find Blackboard", () => {
-    fail("Runtime does not exist");
+    then(/^Runtime loads (.*)$/, (arg0) => {
+      fail("Not yet implemented");
+    });
+
+    and(/^Runtime starts (.*)$/, (arg0) => {
+      fail("Not yet implemented");
+    });
   });
 });
