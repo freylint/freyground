bb9ad01edd97a1ff6c5388632509356723cdff4c
freylint <lmpriestley@tuta.io>
2022-12-08 16:32:40 -0500

    Refactor gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,10 @@ const gulp = require('gulp');
 var jest = require('gulp-jest').default;
 var exec = require('child_process').exec;
 
-function defaultTask(cb) {
-  smiBuildRust(cb);
-  acceptanceTests(cb);
-  cb();
-}
 
+/// Acceptance testing gulp task
 function acceptanceTests(cb) {
-  return gulp.src("./tests/").pipe(jest({
+  return gulp.src("./tests/*.steps.js").pipe(jest({
     "preprocessorIgnorePatterns": [
       "<rootDir>/dist/", "<rootDir>/node_modules/"
     ],
@@ -20,20 +16,33 @@ function acceptanceTests(cb) {
   );
 }
 
-function smiBuildRust(cb) {
-  return gulp.src("projects/fg-smi").pipe(
+/// Standard Messaging Interface Rust bindings generation task
+function smiGenRust(cb) {
+  return gulp.src("projects/fg-smi/*.wit").pipe(
     exec("wit-bindgen guest rust projects/fg-smi/smi.wit --out-dir build/gen/",
-      (err, stdout, stderr) => {
-        if (err === null) {
-          console.log(stdout);
-          console.warn(stderr)
-        } else {
-          console.error(err);
-          process.exit(-1);
-        }
-      }
+      handleShErr
     ))
 }
 
+/// Standard Messaging Interfact Rust bindings build task
+function smiBuildRust(cb) {
+  return gulp.src("build/gen/smi.rs").pipe(
+    exec("rustc --out-dir build/int/ build/gen/smi.rs \
+          --crate-type cdylib --target wasm32-unknown-unknown",
+      handleShErr
+    )
+  )
+}
 
-exports.default = defaultTask;
+exports.default = gulp.series(smiGenRust, smiBuildRust, acceptanceTests);
+
+/// Standard error handling fn for raw `exec()` tasks.
+function handleShErr(err, stdout, stderr) {
+  if (err === null) {
+    console.log(stdout);
+    console.warn(stderr)
+  } else {
+    console.error(err);
+    process.exit(-1);
+  }
+}
