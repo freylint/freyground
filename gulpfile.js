4aea6748d1d39514457903b140f7e8f0ec8d8321
freylint <lmpriestley@tuta.io>
2022-12-08 16:14:23 -0500

    Create build script to build smi to rust.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,39 @@
 //! Default buildscript for freyground monorepo.
 
 const gulp = require('gulp');
-
 var jest = require('gulp-jest').default;
+var exec = require('child_process').exec;
 
 function defaultTask(cb) {
-    acceptanceTests(cb);
-    cb();
+  smiBuildRust(cb);
+  acceptanceTests(cb);
+  cb();
 }
 
 function acceptanceTests(cb) {
-    process.env.NODE_ENV = 'test';
-    return gulp.src("./tests/").pipe(jest({
-        "preprocessorIgnorePatterns": [
-            "<rootDir>/dist/", "<rootDir>/node_modules/"
-        ],
-        "automock": false
-    })
-    );
+  return gulp.src("./tests/").pipe(jest({
+    "preprocessorIgnorePatterns": [
+      "<rootDir>/dist/", "<rootDir>/node_modules/"
+    ],
+    "automock": false
+  })
+  );
 }
 
+function smiBuildRust(cb) {
+  return gulp.src("projects/fg-smi").pipe(
+    exec("wit-bindgen guest rust projects/fg-smi/smi.wit --out-dir build/gen/",
+      (err, stdout, stderr) => {
+        if (err === null) {
+          console.log(stdout);
+          console.warn(stderr)
+        } else {
+          console.error(err);
+          process.exit(-1);
+        }
+      }
+    ))
+}
 
 
 exports.default = defaultTask;
