5404fc18c8de9e3ee3ab7ab9bb0ef9dfa4b263c8
Freylint <lmpriestley@protonmail.com>
2022-12-27 11:46:38 -0500

    Update smi to use wasm-pack for build.

--- packages/smi/src/lib.rs
+++ packages/smi/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use wasm_bindgen::prelude::*;
+
+/// Message for checking if running applications are still responding.
+#[wasm_bindgen]
+pub enum KeepAliveMsg {
+    Ping,
+    Pong
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
