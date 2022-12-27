834692ba286cce31f451c7853bcb2e25d9b6ea74
Freylint <lmpriestley@protonmail.com>
2022-12-27 00:14:23 -0500

    Impl basic service manifest for runtime.

--- projects/fg-runtime/src/main.rs
+++ projects/fg-runtime/src/main.rs
@@ -3,8 +3,49 @@
 //! This handles the loading, and execution of the various wasm components
 //! of the greater freyground repository.
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+#![warn(clippy::all)]
 
+use wasmer::{Store, Module, Instance, Value, imports};
+use wasmer::FunctionEnv;
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
+}
+mod wasm_load {
+    //! Wasm module loading logic
+    
+    /// A LUT of file paths to load services from.
+    #[derive(Debug, PartialEq)]
+    pub(crate) struct ServiceManifest<'manifest> {
+        fg_smi: &'manifest str,
+    }
+
+    impl<'manifest> ServiceManifest<'manifest> {
+        fn new(smi: &'manifest str) -> Self{
+            Self {
+                fg_smi: smi,
+            }
+        }
+    }
+
+    impl Default for ServiceManifest<'static> {
+        fn default() -> ServiceManifest<'static> {
+            Self::new("services/fg_smi.wasm")
+        }
+    }
+    
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn srv_man_constructors() {
+            let man = ServiceManifest::new("services/fg_smi.wasm");
+
+            assert_eq!(man, ServiceManifest::default());
+        }
+    }
 }
