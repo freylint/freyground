dd793de4da74487355a5d0231aa087e25a80e10e
Freylint <lmpriestley@protonmail.com>
2022-12-29 02:28:29 -0500

    Load SMI wasm mod in acceptance tests.

--- packages/acceptance/src/smi.steps.js
+++ packages/acceptance/src/smi.steps.js
@@ -1,37 +1,65 @@
 //! This is a set of Jest tests which test the SMI using it's autodocs.
 
 import { defineFeature, loadFeature } from "jest-cucumber";
-import {KeepAliveMsg} from "../../../build/dist/lib/smi/smi";
+const fs = require("fs");
+const path = require("path");
 
-/// The cucumber feature to be tested
+/// The cucumber feature to for testing
 const feature = loadFeature("features/smi.feature");
 
-defineFeature(feature, (test) => {
-  test("The runtime needs to know if a service is running", ({ then, and }) => {
-    then("SMI provides keepalive_ping", () => {
-      KeepAliveMsg.Ping;
-    });
+/// Boostrap the WASM module being tested
+describe("SMI Tests", () => {
+  let smi;
 
-    and("SMI provides keepalive_pong", () => {
-      KeepAliveMsg.Pong;
+  beforeAll(async () => {
+    const wasmPath = path.resolve(
+      __dirname,
+      "../../../build/cache/wasm32-unknown-unknown/release/",
+      "smi.wasm"
+    );
+    const buffer = fs.readFileSync(wasmPath);
+    const results = await WebAssembly.instantiate(buffer, {
+      env: {
+        memoryBase: 0,
+        tableBase: 0,
+        memory: new WebAssembly.Memory({ initial: 0 }),
+        table: new WebAssembly.Table({ initial: 4, element: "anyfunc" }),
+        abort: console.log,
+      },
     });
+    smi = results.instance.exports;
   });
 
-  test("A service needs ability to query Blackboard", ({ then }) => {
-    then("SMI provides blackboard_address_request", () => {
-      notYetImplemented();
-    });
-  });
+  defineFeature(feature, (test) => {
+    test("The runtime needs to know if a service is running", ({
+      then,
+      and,
+    }) => {
+      then("SMI provides keepalive_ping", () => {
+        smi.KeepAliveMsg.Ping;
+      });
 
-  test("service and blackboard need to communicate", ({ then }) => {
-    then("SMI provides blackboard_requests enum", () => {
-      notYetImplemented();
+      and("SMI provides keepalive_pong", () => {
+        smi.KeepAliveMsg.Pong;
+      });
     });
-  });
 
-  test("service needs to respond to request from blackboard", ({ then }) => {
-    then("SMI provides blackboard_responses variant", () => {
-      notYetImplemented();
+    test("A service needs ability to query Blackboard", ({ then }) => {
+      then("SMI provides blackboard_address_request", () => {
+        notYetImplemented();
+      });
+    });
+
+    test("service and blackboard need to communicate", ({ then }) => {
+      then("SMI provides blackboard_requests enum", () => {
+        notYetImplemented();
+      });
+    });
+
+    test("service needs to respond to request from blackboard", ({ then }) => {
+      then("SMI provides blackboard_responses variant", () => {
+        notYetImplemented();
+      });
     });
   });
 });
