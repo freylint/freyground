82f7a66a9e99282d5af80e7e82515cb50d4f02ab
Lucas Priestley <lmpriestley@outlook.com>
2023-07-28 22:22:22 -0400

    Move background from Hero to its own component.

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -1,12 +1,12 @@
 //! Hero / SplasPage designed for website homepages
 
-import "./Hero.css";
+import Background from "../Background";
 
 export default function Hero() {
   return (
-    <div className="hero min-h-screen bg-base-200 splash-grid">
-      <div className="hero-overlay " />
-      <div className="hero-content text-center">
+    <div className="min-h-screen hero bg-base-200 splash-grid">
+      <Background />
+      <div className="text-center hero-content">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Welcome to the RelayGround!</h1>
           <p className="py-6">
