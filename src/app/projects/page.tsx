82c8ba985777b6ca86c46f6c3d3efe6f2d4b1bb7
Lucas Priestley <lmpriestley@outlook.com>
2023-07-28 22:23:49 -0400

    Add background to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,10 @@
+import Background from "../components/Background";
+
 export default function ProjectPage() {
   return (
     <main>
       <p>Projects page</p>
+      <Background />
     </main>
   );
 }
