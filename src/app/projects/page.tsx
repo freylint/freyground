e6d891906cb6b4f57613700149f16bd47ca4c9cc
Lucas Priestley <lmpriestley@outlook.com>
2023-08-01 23:32:20 -0400

    Scaffold command mapping algorithm for the CLI trainer.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,11 @@
 import Background from "../components/Background";
+import CLITrainer from "./CLITrainer";
 
 export default function ProjectPage() {
   return (
     <main>
       <p>Projects page</p>
+      <CLITrainer />
       <Background />
     </main>
   );
